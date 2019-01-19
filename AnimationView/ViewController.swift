30d09628ad088963de2420ba29e01d86b8865a84
Gianmarco Cotellessa <g.cotellessa@betacom.it>
2019-01-19 21:08:37 +0100

    wip for animation view

--- AnimationView/ViewController.swift
+++ AnimationView/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
+    
+    var snowView: SnowView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        snowView = SnowView(frame: CGRect(x: -150, y: -100, width: 300, height: 50))
+        let snowClipView = UIView(frame: view.frame.offsetBy(dx: 0, dy: 0))
+        snowClipView.clipsToBounds = true
+        snowClipView.addSubview(snowView)
+        view.addSubview(snowClipView)
+    
     }
-
-
+    
 }
 
